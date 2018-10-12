832786903acc40863e99575fef1aeeae138f2175
zr9558 <zr9558@gmail.com>
2018-10-12 23:29:54 +0800

    ---

--- time_series_analysis/test.py
+++ time_series_analysis/test.py
@@ -1,2 +1,13 @@
 import numpy as np
+import pandas as pd
+import tushare as ts
+
+# df = ts.get_tick_data('000581',date='2018-01-09')
+df = ts.get_realtime_quotes('000581')
+print(df)
+
+df = ts.get_hist_data('000581')
+
+df.plot()
+
 print(np.array(10))
